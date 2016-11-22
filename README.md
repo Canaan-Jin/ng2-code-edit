4b6051bcaf2021a85181b79e10326712cd448bef
Canaann_JIn <noconnection@vip.qq.com>
2016-11-22 22:32:11 +0800

    构建一个可运行的angular2 环境

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ng2-code-edit
 这是一个在angular2 环境下的一个 代码编辑器组件
+
+# 如何运行
+安装依赖
+```
+npm install
+```
+编译环境
+```
+npm run build
+```
+运行起来
+```
+npm run lite
+```
---
+++ src/component/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    template: `this this project is a code editor in the angularjs2's directive`
+})
+export class AppComponent { }
